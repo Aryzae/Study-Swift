a1b7947d96060c7755cd08cd1c63dae2d282f9f4
Aryzae <a.aryzae@live.jp>
2017-09-30 23:44:20 +0900

    Initial commit

---
+++ TwitterKitTest/TwitterKitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterKitTest
+//
+//  Created by Aryzae on 2017/09/30.
+//  Copyright © 2017年 Aryzae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
